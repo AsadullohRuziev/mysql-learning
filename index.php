b47b6c9a22d4ebdcb9ca639157a3e27e31bc080a
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-31 15:27:47 +0500

    Insert Data Into MySQL

--- index.php
+++ index.php
@@ -31,18 +31,12 @@ echo "Connected successfully";
 
 // sql to create table
 
-$sql = "CREATE TABLE mysqltable(
-       id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-       firstname VARCHAR(30) NOT NULL,
-       lastname VARCHAR(30) NOT NULL,
-       email VARCHAR(50) NOT NULL,
-       reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
- )";
+$sql = "INSERT INTO mysqltable(firstname, lastname, email) VALUES('Jek', 'Deep', 'jek@deep.com')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
+    echo "New record created successfully";
 } else {
-    echo "Error creating table: " . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
